4dd05c1b0c3e342d695a6a449c0567b204129d5a
edmond buck <edmond.buck@wearetissue.com>
2023-02-20 17:09:36 -0800

    Debugging

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tp-generator-db
+
+# Description
+A team profile generator is a tool that helps you create professional profiles for your team members. You can use it to easily add information like names, job titles, bios, and contact information. A team profile generator helps you showcase the skills and expertise of your team and is a great way to make a good first impression.![Alt text](img/Screenshot%202023-02-16%20at%2011.44.09%20PM.png)
+
+# Link to Walk-through Video
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 
 //These lines of code import the Employee, Manager, Engineer, and Intern classes from the "lib" folder, as well as the generateHtml function from the "utils" folder, which are likely being used to build an HTML page for a software application or website.
 const Employee = require("./lib/Employee");
-const EngineeringDirector = require("./lib/EngineeringDirector");
-const SeniorDeveloper = require("./lib/SeniorDeveloper");
-const JuniorEngineer = require("./lib/jJuniorEngineer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const generateHtml = require("./utils/generatehtml");
 
 //This line of code declares an empty array called "employees", which can be used to store and manipulate data related to employee records in a program or application.
@@ -16,7 +16,7 @@ const employees = [];
 //This line of code declares a function called "writeToFile" that takes in two parameters: "fileName" (a string representing the name of the file to be written to) and "data" (the data to be written to the file). The function is likely being used to write data to a file in a Node.js application.
 function writeToFile(fileName, data) {
 
-    s.writeFile(fileName, generateHtml(employees), (err) =>
+    fs.writeFile(fileName, generateHtml(employees), (err) =>
     err ? console.error(err) : console.log("index.html successfully created!")
   );
 }
